768da433d1180dd381b74ef4339bf79ef258449d
Yunus Emre Benli <72562091+Himera19@users.noreply.github.com>
2024-10-29 20:00:57 +0300

    code cleaned

--- Big Calculator.cpp
+++ Big Calculator.cpp
@@ -1,67 +1,56 @@
-#include<stdio.h>
-#include<conio.h>
-#include<math.h>
-int main()
-{
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
 
+int main() {
+    printf("Welcome to Big Calculator:\n");
 
+    // Drawing a star pattern
+    int a, b;
+    for (a = 0; a <= 5; a++) {
+        for (b = 0; b <= a; b++) {
+            printf("*");
+        }
+        printf("\n");
+    }
 
-{
-	printf("Welcome to Big Calculator:\n");
-}
-	int a,b;
-	for (a=0;a<=5;a++){
-	for(b=0;b<=a;b++){
-	
-	printf("*");
-}
-printf("\n");
-}
-	
-	float sayi;
-	printf("Please write a number: ");
-	scanf("%f",&sayi);
-    printf("Square of the number you entered:  %.2f",sqrt(fabs(sayi)));
-    printf("\nThe exp value of the number you entered:  %.2f",exp(sayi));
-	printf("\nThe logarithmic value you entered:  %.2f",log(sayi));
-	printf("\nThe base value of the number you entered: %.2f",floor(sayi));
-	printf("\nThe sinus value you entered %.2f\n",sin(sayi));
-	
-	printf("\n*** Now It's Time for Our Calculator ***\n");
-	{
-	int sayi1, sayi2, toplama, cikarma;
-	printf("\nEnter first number: ");
-	scanf("%d",&sayi1);
-	printf("\nEnter second number: ");
-	scanf("%d",&sayi2);
-	toplama=sayi1+sayi2;
-	cikarma=sayi1-sayi2;
-	printf("\nSum of Numbers: %d'dir",toplama);
-	printf("\nDifference of Numbers: %d'dir\n",cikarma);
-	}
-	
-	{
-	printf("\n***Did you like it ?***");
-}
-{
-printf("For Yes =1 For No=0: ");
-}
-	
-    int x; 
-    scanf("%d",&x); 
-    if( x>=1 ) 
-    { 
-        printf("Thank you so much :) "); 
-    } 
-    else 
-    { 
-        printf("You broke my heart :'( "); 
-}
+    // Mathematical operations
+    float number;
+    printf("Please write a number: ");
+    scanf("%f", &number);
+    printf("Square root of the entered number: %.2f\n", sqrt(fabs(number)));
+    printf("Exponential value of the entered number: %.2f\n", exp(number));
+    printf("Logarithmic value of the entered number: %.2f\n", log(number));
+    printf("Floor value of the entered number: %.2f\n", floor(number));
+    printf("Sine value of the entered number: %.2f\n", sin(number));
 
-{
+    printf("\n*** Now It's Time for Our Calculator ***\n");
 
-printf("\n***By Yunus Emre Benli***");
-}	
-getch ();
-	return 0;
+    // Addition and subtraction operations
+    int number1, number2, sum, difference;
+    printf("\nEnter first number: ");
+    scanf("%d", &number1);
+    printf("Enter second number: ");
+    scanf("%d", &number2);
+    sum = number1 + number2;
+    difference = number1 - number2;
+    printf("\nSum of Numbers: %d\n", sum);
+    printf("Difference of Numbers: %d\n", difference);
+
+    // User feedback
+    printf("\n*** Did you like it? ***\n");
+    printf("For Yes = 1, For No = 0: ");
+
+    int feedback;
+    scanf("%d", &feedback);
+    if (feedback >= 1) {
+        printf("Thank you so much :) ");
+    } else {
+        printf("You broke my heart :'( ");
+    }
+
+    printf("\n*** By Yunus Emre Benli ***\n");
+
+    getch();
+    return 0;
 }
